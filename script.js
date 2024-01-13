f1e7172309896c1c7a4d52b2e54a05548e29e0d4
Vinny Murphy <vinnywebdev@gmail.com>
2024-01-13 11:58:52 +0000

    completed exercise 3

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ console.log("Working");
     Remove the property name from the object.
 */
 let user = {};
-
 user.name = "John";
 user.surname = "Smith";
 user.name = "Pete";
@@ -17,9 +16,8 @@ console.log(user);
 
 /*Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.*/
 let empty = {};
-
-console.log(Object.keys(empty).length);
-
+//Object.keys returns an array of the key sin an object
+//If the array length is zero, the object is empty
 function isEmpty(userObj) {
   if (Object.keys(userObj).length == 0) {
     return true;
@@ -29,3 +27,37 @@ function isEmpty(userObj) {
 }
 
 console.log(isEmpty(empty));
+
+//another way of achieveing the same outcome
+function isEmpty(obj) {
+  for (let key in obj) {
+    // if the loop has started, there is a property
+    return false;
+  }
+  return true;
+}
+
+/*Write the code to sum all salaries and store in the variable sum. If salaries is empty, then the result must be 0.*/
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+function sumObj(obj) {
+  let total = 0;
+  //if the object is empty return 0
+  if (Object.keys(obj).length == 0) {
+    console.log(total);
+    return total;
+  }
+  for (let key in obj) {
+    /*Access the property value using square bracket notation
+    it has to be brackets in order for this to be dynamic.
+    if we say obj.key the function looks for a specific key named "key"*/
+    console.log(obj[key]);
+    total += obj[key];
+  }
+  console.log(total);
+}
+sumObj(salaries);
